eb5cdb6edcfa759f91aab9c7a32eb04a864dbe2f
aerol <alparslan.erol@boun.edu.tr>
2020-04-30 14:13:07 +0300

    Interface and Main classes are commented out!

--- src/gui/Interface.java
+++ src/gui/Interface.java
@@ -5,26 +5,36 @@ import javax.swing.JFrame;
 public class Interface extends JFrame {
 
     /**
-	 * 
-	 */
+     * Top class of this game which extends JFrame class.
+     * General specification of the game window are held in this class.
+     * Interface class HAS An another class that suppose to the game engine.
+     * HAS Main class with an object of 'manager'.
+     */
 	private static final long serialVersionUID = 1L;
 	public static int WIDTH = 800;
     public static int HEIGHT = 500;
     private Main manager;
 
-
+    /**
+     * Constructor...
+     * Configure general concepts of the JFrame window.
+     * Create an object from Main class and add this object to the window.
+     */
     public Interface() {
         super("DINO - GAME");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(WIDTH,HEIGHT);
-        setLocation(400,200);
-        setResizable(true);
-
-        manager = new Main();
-        addKeyListener(manager);
-        add(manager);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Finish program click on exit button.
+        setSize(WIDTH,HEIGHT); // WIDTH and HEIGHT CONFIGURATION
+        setLocation(400,200); // SET WINDOW LOCATION ONT THE SCREEN
+        setResizable(false); // DEFAULT (FALSE), CASE TRUE => YOU CAN RESIZE THE WINDOW AS YOU WISH.
+        manager = new Main(); // CREATE MAIN OBJECT THAT USE THREAD RUNNABLE
+        addKeyListener(manager); //ADD KEY LISTENER BY OVERRRIDE METHODS IN KEYLISTENER INTERFACE
+        add(manager); // ADD PROCESS ON THE SCREEN AND DISPLAY
     }
 
+    /**
+     * This method is to trigger the game engine. Also called the THREAD with startGame() method.
+     * And set visible process maden by manager object on our game window.
+     */
     public void startGame() {
         setVisible(true);
         manager.startGame();
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -14,8 +14,18 @@ import classes.Player;
 public class Main extends JPanel implements KeyListener, Runnable {
 
     /**
-	 * 
-	 */
+     * MAIN CLASS ==> Engine of the Dinosaur Game
+     *
+     * Main class HAS 3 Subclasses : CACTUS, GROUND, PLAYER
+     * Main class HAS THREAD class with thread object.
+     * Input images identified as BufferedImage class type to read them as BufferedImage type later.
+     *
+     * Game has 3 Stage:
+     * 1.) Start Game Stage    : In this stage, dino does not run. Position just before the running start. (INITIAL)
+     * 2.) Game Playing State  : In this stage, dino is running relatively :)
+     * 3.) Game Over Stage     : Every thread, program checks the collision between dino and cactus with a reference of
+     *                           Rectangle Class. If it is the case pursuing this stage with stop running.
+     */
 	private static final long serialVersionUID = 1L;
 	public static int WIDTH;
     public static int HEIGHT;
@@ -25,20 +35,24 @@ public class Main extends JPanel implements KeyListener, Runnable {
     private static final int GAME_OVER_STATE = 2;
     private int gameState = START_GAME_STATE;
 
-    private static BufferedImage replayButtonImage;
-    private static BufferedImage gameOverButtonImage;
+    private BufferedImage replayButtonImage;
+    private BufferedImage gameOverButtonImage;
 
-    private Thread animator;
+    private Thread thread;
     private Ground ground;
     private Cactus cactus;
     private Player dino;
 
 
+    /**
+     * CONSTRUCTOR OF MAIN CLASS
+     * Initialize sub class objects and read the game over images.
+      */
     public Main() {
         WIDTH = Interface.WIDTH;
         HEIGHT = Interface.HEIGHT;
 
-        animator = new Thread(this);
+        thread = new Thread(this);
         ground = new Ground(HEIGHT);
         dino = new Player();
         cactus = new Cactus(WIDTH*2, dino);
@@ -47,9 +61,18 @@ public class Main extends JPanel implements KeyListener, Runnable {
         replayButtonImage = Resource.getResourceImage("./images/replay_button.png");
     }
 
+
+    /**
+     * Using Java Graphics to draw initial update on the game for every single GAME_STATE.
+     * By calling repaint in run() method calls this method for every single iteration in thread. So we are allowed
+     * to update drawing image in the window in every thread iteration with specified sleep time.
+     *
+     * @param g
+     */
     public void paint(Graphics g) {
         super.paint(g);
         g.setFont(new Font("Courier New", Font.BOLD, 15));
+        g.drawString("PRESS L BUTTON TO QUIT", 10,20);
         g.drawString("SCORE = " + dino.score, getWidth()/2 + 200, getHeight()/4-25);
         g.drawString("High Score  = " + dino.high_score, getWidth()/2 + 200, getHeight()/4);
 
@@ -76,11 +99,21 @@ public class Main extends JPanel implements KeyListener, Runnable {
         }
     }
 
+    /**
+     * Overridden method run() from Interface Runnable
+     * Thread class also implements runnable interface which means Thread itself has an override run() method.
+     * So we are allowed to use Thread class into Runnable run() method.
+     * This method is called for every single iteration.
+     *
+     * @code updateGame() ==> In every thread we control and trigger Main class method updateGame() to Up Date our game.
+     * @code repaint() ==> This method is overridden inherited from JPanel but also from COMPONENT class to Update Graphics ==> paint(Graphics g)
+     * @see #update(Graphics)
+     */
     @Override
     public void run() {
         while (true) {
-            updateGame();
-            repaint();
+            updateGame(); //Main class method
+            repaint(); // Inherited from JPanel ==> COMPONENT
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
@@ -89,11 +122,28 @@ public class Main extends JPanel implements KeyListener, Runnable {
         }
     }
 
+    /**
+     * This method is overridden from...
+     * @code KeyListener interface.
+     * Chcek if any key TYPED.
+     * @param e -- any key
+     */
     @Override
     public void keyTyped(KeyEvent e) {
 
     }
 
+    /**
+     * This method is overridden from...
+     * @code KeyListener interface.
+     * Chcek if any key PRESSED.
+     * In this game only key that allowed to use is ' '.
+     * For every GAME_STATE, written key cases.
+     * - IF IN START_GAME_STATE AND PRESSED ' ' => Dino stars running and GAME_STATE => GAME_PLAYING_STATE
+     * - IF IN GAME_PLAYING_STATE AND PRESSED ' ' => Dino jumps and no state change
+     * - IF IN GAME_OVER_STATE AND PRESSED ' ' => Dino has initial posiiton and GAME_STATE => START_GAME_STATE
+     * @param e -- any key
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == ' ') {
@@ -111,11 +161,27 @@ public class Main extends JPanel implements KeyListener, Runnable {
                     break;
             }
         }
+        if (e.getKeyCode() == KeyEvent.VK_L) {
+            stopGame();
+        }
     }
 
+    /**
+     * This method is overridden from...
+     * @code KeyListener interface.
+     * Chcek if any key RELEASED.
+     * @param e -- any key
+     */
     @Override
     public void keyReleased(KeyEvent e) { }
 
+    /**
+     * Method for Main class.
+     * Has no parameter and no return
+     * This method updates game process but to make it every single thread (iteration)...
+     * This method is called from...
+     * @code overridden run() method. So we are allowed to update game process in every single thread iteration.
+     */
     public void updateGame() {
         if (gameState == GAME_PLAYING_STATE) {
             ground.update();
@@ -134,6 +200,11 @@ public class Main extends JPanel implements KeyListener, Runnable {
         }
     }
 
+    /**
+     * Method for Main class.
+     * Has no input parameter and no return.
+     * This method is used for re-setting the game configuration.
+     */
     public void reset() {
         System.out.println("Reset");
         cactus.reset();
@@ -142,7 +213,22 @@ public class Main extends JPanel implements KeyListener, Runnable {
         dino.counter = 0;
     }
 
+    /**
+     * @code startGame()
+     * @code stopGame()
+     *
+     * Methods for Main class.
+     * Have no input parameter and no return.
+     * This startGame() method is used Thread -> start() method which used to trigger the thread run().
+     * In thread we do not call run() method directly. Just call start() method and iteration starts till it is ended.
+     * When you invoke the program and get the game window. It means thread has already started even the dino has start_game_state.
+     *
+     * You can stop it either use stopGame() method from Main class or just click on the exit window button the program has been finished.
+     */
     public void startGame() {
-        animator.start();
+        thread.start();
+    }
+    private void stopGame() {
+        System.exit(1);
     }
 }
